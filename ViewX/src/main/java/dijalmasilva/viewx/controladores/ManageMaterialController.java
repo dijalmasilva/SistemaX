e31b4256f54dca156e74e3dc29b990514147f4fa
dijalmasilva <dijalmacz@gmail.com>
2016-04-20 15:24:23 -0300

    Cadastrando material

--- ViewX/src/main/java/dijalmasilva/viewx/controladores/ManageMaterialController.java
+++ ViewX/src/main/java/dijalmasilva/viewx/controladores/ManageMaterialController.java
@@ -6,9 +6,12 @@
 
 package dijalmasilva.viewx.controladores;
 
+import dijalmasilva.bussinessx.entidades.Material;
+import dijalmasilva.integrationx.gerenciadores.GerenciaMaterial;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 
@@ -20,6 +23,19 @@ public class ManageMaterialController {
 
     @RequestMapping("")
     public String managerMaterial(HttpServletRequest req){
+        GerenciaMaterial gm = new GerenciaMaterial();
+        req.getSession().setAttribute("materials", gm.todos());
         return "gerenciadorMaterial";
+    }
+    
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addMaterial(Material m, HttpServletRequest req){
+        GerenciaMaterial gm = new GerenciaMaterial();
+        boolean salvou = gm.salvar(m);
+        if (salvou){
+            req.setAttribute("result", "Material salvo com sucesso!");
+        }
+        
+        return managerMaterial(req);
     }
 }
